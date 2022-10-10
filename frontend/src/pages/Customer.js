b3af8ab24781c28f01902266d8f7230a82ba2343
JY-GitTech <josua.tech@gmail.com>
2022-10-11 00:37:10 +0700

    add customer sidebar

--- frontend/src/pages/Customer.js
+++ frontend/src/pages/Customer.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import SideBar from "../components/SideBar";
+import "../assets/css/customer.css";
 
 const Customer = () => {
-    return (
-        <div>Customer</div>
-    )
-}
+  return (
+    <div className="customer">
+      <SideBar />
+    </div>
+  );
+};
 
-export default Customer
+export default Customer;
